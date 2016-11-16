b0050f85c83c2b135fbe878cd0e2e15244cb47cd
Jeff Rose <jeff@thinktopic.com>
2016-11-16 10:35:39 -0700

    Adding support for media queries and keyframe rules.  Outputting Garden data rather than instances too.

--- src/greenhouse/css.clj
+++ src/greenhouse/css.clj
@@ -5,11 +5,13 @@
            [com.helger.css.decl CSSExpressionMemberFunction CSSExpressionMemberMath
                                 CSSExpressionMemberTermSimple CSSExpressionMemberTermURI
                                 ECSSExpressionOperator CSSSelectorAttribute
-                                CSSSelectorSimpleMember]
+                                CSSSelectorSimpleMember CSSMediaQuery]
            [com.helger.css.decl.visit CSSVisitor DefaultCSSVisitor]
            [com.helger.css.writer CSSWriterSettings])
   (:require [clojure.java.io :as io]
             [garden.selectors :refer [attr]]
+            [garden.core :as garden]
+            [garden.stylesheet :refer [at-media at-keyframes]]
             [camel-snake-kebab.core :refer [->kebab-case]]))
 
 (def libs* (atom {}))
@@ -26,20 +28,12 @@
 (defmethod parse-selector-member CSSSelectorAttribute
   [m]
   (if-let [op (.getOperator m)]
-    (attr (.getAttrName m) (as-css op) (.getAttrValue m))
-    (attr (.getAttrName m))))
-
-;(defmethod parse-selector-member CSSSelectorMemberFunctionLike
-;  [m])
-;(defmethod parse-selector-member CSSSelectorMemberNot
-;  [m])
+    (list 'attr (.getAttrName m) (as-css op) (.getAttrValue m))
+    (list 'attr (.getAttrName m))))
 
 (defmethod parse-selector-member CSSSelectorSimpleMember
   [m]
   (keyword (as-css m)))
-
-;(defmethod parse-selector-member ECSSSelectorCombinator
-;  [m])
 
 (defmethod parse-selector-member :default
   [m]
@@ -59,26 +53,6 @@
   [m]
   (as-css m))
 
-;(defmethod parse-expression-member CSSExpressionMemberFunction
-;  [m]
-;  (as-css m))
-;
-;(defmethod parse-expression-member CSSExpressionMemberMath
-;  [m]
-;  (as-css m))
-;
-;(defmethod parse-expression-member CSSExpressionMemberTermSimple
-;  [m]
-;  (as-css m))
-;
-;(defmethod parse-expression-member CSSExpressionMemberTermURI
-;  [m]
-;  (as-css m))
-;
-;(defmethod parse-expression-member ECSSExpressionOperator
-;  [m]
-;  (as-css m))
-
 (defn parse-expression
   "Parses the value side of a declaration.
   e.g.
@@ -90,11 +64,37 @@
   (let [members (map parse-expression-member (.getAllMembers expr))]
     (apply str (interpose " " members))))
 
+(defn parse-media-query
+  [q]
+  (let [medium (.getMedium q)
+        med-kw (when medium
+                     (keyword (.toLowerCase medium)))
+        modifier (.getModifier q)
+        mod-kw (if (not= modifier com.helger.css.decl.CSSMediaQuery$EModifier/NONE)
+                 (keyword (.toLowerCase (.getCSSText modifier)))
+                 false)
+        media (if medium
+                {med-kw (or mod-kw true)}
+                {})
+        expressions (into media (map (fn [expr]
+                                       [(keyword (.toLowerCase (.getFeature expr))) (parse-expression (.getValue expr))])
+                                     (.getAllMediaExpressions q)))]
+    expressions))
+
+(defn parse-media-rule
+  [query-rule]
+  (let [queries (.getAllMediaQueries query-rule)]
+    (at-media (apply merge (map parse-media-query queries)))))
+
 (defn css->garden-visitor
   []
   (let [state* (atom {:styles [] ; accumulates all of the parsed styles
                       :selectors nil ; holds the current rule's selectors
                       :classes {} ; accumlutes all the single class styles
+                      :media-rule nil ; keep the current media query
+                      :media-styles nil ; stash styles here when accumulating styles within media rule
+                      :keyframe-rule nil ; keep the current keyframe rule
+                      :keyframe-blocks []
                       })]
     (proxy [DefaultCSSVisitor PGarden] []
       (onBeginStyleRule [rule]
@@ -113,6 +113,38 @@
                                         declarations)
                                  classes)]
                    (assoc state :styles styles :classes classes)))))
+
+      (onBeginMediaRule [rule]
+        (swap! state*
+               (fn [{:keys [styles] :as state}]
+                 (assoc state
+                        :media-rule (parse-media-rule rule)
+                        :media-styles styles
+                        :styles []))))
+
+      (onEndMediaRule [rule]
+        (swap! state*
+               (fn [{:keys [media-rule media-styles styles] :as state}]
+                 (assoc state
+                        :styles (conj media-styles (apply list 'at-media (get-in media-rule [:value :media-queries]) styles))
+                        :media-rule nil
+                        :media-styles nil))))
+
+      (onBeginKeyframesRule [rule])
+      (onEndKeyframesRule [rule]
+        (swap! state*
+               (fn [{:keys [keyframe-rule keyframe-blocks styles] :as state}]
+                 (assoc state
+                        :styles (conj styles (apply list 'at-keyframes (keyword (.toLowerCase (.getAnimationName rule)))
+                                                    keyframe-blocks))
+                        :keyframe-blocks []))))
+
+      (onBeginKeyframesBlock [block])
+      (onEndKeyframesBlock [block]
+        (swap! state*
+               (fn [{:keys [keyframe-blocks declarations] :as state}]
+                   (assoc state :keyframe-blocks (conj keyframe-blocks [(keyword (.toLowerCase (first (.getAllKeyframesSelectors block))))
+                                                                        declarations])))))
 
       (onDeclaration [decl]
         (swap! state* assoc-in [:declarations (keyword (.getProperty decl))]
@@ -142,6 +174,14 @@
                                         CCharset/CHARSET_UTF_8_OBJ
                                         ECSSVersion/CSS30)]
     (parse-css-dom css-dom)))
+
+(defn css->garden
+  [css]
+  (:styles (parse-css-str css)))
+
+(defn css-file->garden
+  [path]
+  (css->garden (slurp path)))
 
 (defn import-file
   "Import the classes from a CSS file.
